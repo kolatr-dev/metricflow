fbd9b5efba7e2c710bca8b9496a19df00981f39a
Paul Yang <github@ardorful.com>
2023-11-17 16:11:50 -0800

    Add SpecPattern classes.
    Please see the following feature request:
    
    https://github.com/dbt-labs/metricflow/issues/887

---
+++ metricflow/specs/patterns/entity_link_pattern.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+
+import logging
+from dataclasses import asdict, dataclass
+from enum import Enum
+from typing import List, Optional, Sequence, Tuple
+
+from dbt_semantic_interfaces.call_parameter_sets import (
+    DimensionCallParameterSet,
+    EntityCallParameterSet,
+    TimeDimensionCallParameterSet,
+)
+from dbt_semantic_interfaces.references import EntityReference
+from dbt_semantic_interfaces.type_enums import TimeGranularity
+from dbt_semantic_interfaces.type_enums.date_part import DatePart
+from typing_extensions import override
+
+from metricflow.specs.patterns.spec_pattern import SpecPattern
+from metricflow.specs.specs import LinkableInstanceSpec, LinkableSpecSet
+
+logger = logging.getLogger(__name__)
+
+
+class ParameterSetField(Enum):
+    """The fields of the EntityLinkPatternParameterSet class used for matching in the EntityLinkPattern.
+
+    Considering moving this to be a part of the specs module / classes.
+    """
+
+    ELEMENT_NAME = "element_name"
+    ENTITY_LINKS = "entity_links"
+    TIME_GRANULARITY = "time_granularity"
+    DATE_PART = "date_part"
+
+
+@dataclass(frozen=True)
+class EntityLinkPatternParameterSet:
+    """See EntityPathPattern for more details."""
+
+    # Specify the field values to compare. None can't be used to signal "don't compare" because sometimes a pattern
+    # needs to match a spec where the field is None.
+    fields_to_compare: Tuple[ParameterSetField, ...]
+
+    # The name of the element in the semantic model
+    element_name: Optional[str] = None
+    # The entities used for joining semantic models.
+    entity_links: Optional[Tuple[EntityReference, ...]] = None
+    # Properties of time dimensions to match.
+    time_granularity: Optional[TimeGranularity] = None
+    date_part: Optional[DatePart] = None
+
+
+@dataclass(frozen=True)
+class EntityLinkPattern(SpecPattern):
+    """A pattern that matches group-by-items using the entity-link-path specification.
+
+    The entity link path specifies how a group-by-item for a metric query should be constructed. The group-by-item
+    is obtained by joining the semantic model containing the measure to a semantic model containing the group-by-
+    item using a specified entity. Additional semantic models can be joined using additional entities to obtain the
+    group-by-item. The series of entities that are used form the entity path. Since the entity path does not specify
+    which semantic models need to be used, additional resolution is done in later stages to generate the necessary SQL.
+    """
+
+    parameter_set: EntityLinkPatternParameterSet
+
+    @override
+    def match(self, candidate_specs: Sequence[LinkableInstanceSpec]) -> Sequence[LinkableInstanceSpec]:
+        matching_specs: List[LinkableInstanceSpec] = []
+
+        # Using some Python introspection magic to figure out specs that match the listed fields.
+        keys_to_check = set(field_to_compare.value for field_to_compare in self.parameter_set.fields_to_compare)
+        asdict(self.parameter_set)
+        # Checks that EntityLinkPatternParameterSetField is valid wrt to the parameter set.
+        parameter_set_values = tuple(getattr(self.parameter_set, key_to_check) for key_to_check in keys_to_check)
+
+        for spec in candidate_specs:
+            spec_values = tuple(
+                (getattr(spec, key_to_check) if hasattr(spec, key_to_check) else None) for key_to_check in keys_to_check
+            )
+            if spec_values == parameter_set_values:
+                matching_specs.append(spec)
+
+        return matching_specs
+
+
+@dataclass(frozen=True)
+class DimensionPattern(EntityLinkPattern):
+    """Similar to EntityPathPattern but only matches dimensions / time dimensions.
+
+    Analogous pattern for Dimension() in the object builder naming scheme.
+    """
+
+    @override
+    def match(self, candidate_specs: Sequence[LinkableInstanceSpec]) -> Sequence[LinkableInstanceSpec]:
+        spec_set = LinkableSpecSet.from_specs(tuple(candidate_specs))
+        filtered_specs: Sequence[LinkableInstanceSpec] = spec_set.dimension_specs + spec_set.time_dimension_specs
+        return super().match(filtered_specs)
+
+    @staticmethod
+    def from_call_parameter_set(  # noqa: D
+        dimension_call_parameter_set: DimensionCallParameterSet,
+    ) -> DimensionPattern:
+        return DimensionPattern(
+            parameter_set=EntityLinkPatternParameterSet(
+                fields_to_compare=(
+                    ParameterSetField.ELEMENT_NAME,
+                    ParameterSetField.ENTITY_LINKS,
+                ),
+                element_name=dimension_call_parameter_set.dimension_reference.element_name,
+                entity_links=dimension_call_parameter_set.entity_path,
+            )
+        )
+
+
+@dataclass(frozen=True)
+class TimeDimensionPattern(EntityLinkPattern):
+    """Similar to EntityPathPattern but only matches time dimensions.
+
+    Analogous pattern for TimeDimension() in the object builder naming scheme.
+    """
+
+    @override
+    def match(self, candidate_specs: Sequence[LinkableInstanceSpec]) -> Sequence[LinkableInstanceSpec]:
+        spec_set = LinkableSpecSet.from_specs(tuple(candidate_specs))
+        return super().match(spec_set.time_dimension_specs)
+
+    @staticmethod
+    def from_call_parameter_set(  # noqa: D
+        time_dimension_call_parameter_set: TimeDimensionCallParameterSet,
+    ) -> TimeDimensionPattern:
+        fields_to_compare: List[ParameterSetField] = [
+            ParameterSetField.ELEMENT_NAME,
+            ParameterSetField.ENTITY_LINKS,
+            ParameterSetField.DATE_PART,
+        ]
+
+        if time_dimension_call_parameter_set.time_granularity is not None:
+            fields_to_compare.append(ParameterSetField.TIME_GRANULARITY)
+
+        return TimeDimensionPattern(
+            parameter_set=EntityLinkPatternParameterSet(
+                fields_to_compare=tuple(fields_to_compare),
+                element_name=time_dimension_call_parameter_set.time_dimension_reference.element_name,
+                entity_links=time_dimension_call_parameter_set.entity_path,
+                time_granularity=time_dimension_call_parameter_set.time_granularity,
+                date_part=time_dimension_call_parameter_set.date_part,
+            )
+        )
+
+
+@dataclass(frozen=True)
+class EntityPattern(EntityLinkPattern):
+    """Similar to EntityPathPattern but only matches entities.
+
+    Analogous pattern for Entity() in the object builder naming scheme.
+    """
+
+    @override
+    def match(self, candidate_specs: Sequence[LinkableInstanceSpec]) -> Sequence[LinkableInstanceSpec]:
+        spec_set = LinkableSpecSet.from_specs(tuple(candidate_specs))
+        return super().match(spec_set.entity_specs)
+
+    @staticmethod
+    def from_call_parameter_set(entity_call_parameter_set: EntityCallParameterSet) -> EntityPattern:  # noqa: D
+        return EntityPattern(
+            parameter_set=EntityLinkPatternParameterSet(
+                fields_to_compare=(
+                    ParameterSetField.ELEMENT_NAME,
+                    ParameterSetField.ENTITY_LINKS,
+                ),
+                element_name=entity_call_parameter_set.entity_reference.element_name,
+                entity_links=entity_call_parameter_set.entity_path,
+            )
+        )
