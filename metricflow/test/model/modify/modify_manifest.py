01a2e9192e768a0c31d7faa5a85f835435b1b7cb
Paul Yang <github@ardorful.com>
2023-11-29 19:10:22 -0800

    Add methods to modify a semantic manifest for testing.
    The methods to modify metrics / inputs will be used in later test cases.

---
+++ metricflow/test/model/modify/modify_manifest.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from dbt_semantic_interfaces.implementations.semantic_manifest import PydanticSemanticManifest
+from dbt_semantic_interfaces.transformations.semantic_manifest_transformer import PydanticSemanticManifestTransformer
+from dbt_semantic_interfaces.transformations.transform_rule import SemanticManifestTransformRule
+
+
+def modify_manifest(
+    semantic_manifest: PydanticSemanticManifest,
+    transform_rule: SemanticManifestTransformRule[PydanticSemanticManifest],
+) -> PydanticSemanticManifest:
+    """Following the given transform, returns a modified copy of the given manifest."""
+    transformer = PydanticSemanticManifestTransformer()
+    transformed_manifest = transformer.transform(semantic_manifest, ((transform_rule,),))
+
+    return transformed_manifest
